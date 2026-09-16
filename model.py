53c541e423b28e802fad5612b0d9c8743a966ae3
Jonas Aeschbacher <43812978+aejonas@users.noreply.github.com>
2026-09-16 18:21:24 +0200

    Step 7: gather_true_class_probs

--- model.py
+++ model.py
@@ -51,8 +51,10 @@ def one_hot(labels, num_classes):
     one_hot[np.arange(num_labels), labels] = 1.0
     return one_hot
 
-# Step 7 - gather_true_class_probs (not yet solved)
-# TODO: implement
+# Step 7 - gather_true_class_probs
+def gather_true_class_probs(probs, labels):
+    # TODO: return probs[i, labels[i]] for every row i as a 1D length-N array.
+    return probs[np.arange(len(labels)), labels]
 
 # Step 8 - cross_entropy_loss (not yet solved)
 # TODO: implement
